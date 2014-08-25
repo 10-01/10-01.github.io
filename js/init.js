6da46351f2971f65ea4231bf7238e7c9b044b11c
Haydn Strauss <strauss.haydn@gmail.com>
2014-08-25 15:49:24 -0500

    Updated Homepage with More Details

--- js/init.js
+++ js/init.js
@@ -33,6 +33,84 @@
 			document.body.scrollTop = 0;
 		}
 
+$(document).ready(function() {
+    $(window).keydown(function(event){
+        if(event.keyCode == 13) {
+            event.preventDefault();
+            return false;
+        }
+    });
+});
+$(document).on('click','#signup_beta', function (e) {
+    var email = $("input#inputEmail").val();
+    var url = 'https://us7.api.mailchimp.com/2.0/lists/subscribe.json';
+    var api = 'ed69d1950c819476f294738534df4d6d-us7';
+    var list_id = 'e725d18013';
+    var mc_package = {"apikey":api, "id":list_id, "email":{"email":email}};
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: mc_package,
+        success: function(e) {
+            $('.signup_form').html("<div id='message' class='col-xs-12 col-md-6 col-md-offset-2'></div>");
+
+            $('#message').html("<h4>Thank you for joining our newsletter list!</h4>")
+                .append("<h5>Check your email, confirm it, and you will start seeing exclusive deals and news.</h5>")
+                .hide()
+                .fadeIn(1500, function() {
+                    $('#message').append("<div></div>");
+                });
+
+        },
+        error: function(e) {
+            $('.signup_form').html("<div id='message' class='col-md-6 col-md-offset-3 col-xs-6 col-xs-offset-3'></div>");
+
+            $('#message').html("<h4>Thank you for joining our newsletter list!</h4>")
+                .append("<h5>Check your email, confirm it, and you will start seeing exclusive deals and news.</h5>")
+                .hide()
+                .fadeIn(1500, function() {
+                    $('#message').append("<div></div>");
+                });
+
+        }
+    });
+});
+$(document).on('click','#mobile_signup_beta', function (e) {
+    var email = $("input#mobile_inputEmail").val();
+    var url = 'https://us7.api.mailchimp.com/2.0/lists/subscribe.json';
+    var api = 'ed69d1950c819476f294738534df4d6d-us7';
+    var list_id = 'e725d18013';
+    var mc_package = {"apikey":api, "id":list_id, "email":{"email":email}};
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: mc_package,
+        success: function(e) {
+            $('.mobile_signup_form').html("<div id='message' class='col-xs-12 col-md-6 col-md-offset-2'></div>");
+
+            $('#message').html("<h4>Thank you for joining our newsletter list!</h4>")
+                .append("<h5>Check your email, confirm it, and you will start seeing exclusive deals and news.</h5>")
+                .hide()
+                .fadeIn(1500, function() {
+                    $('#message').append("<div></div>");
+                });
+
+        },
+        error: function(e) {
+            $('.mobile_signup_form').html("<div id='message' class='col-md-6 col-md-offset-3 col-xs-6 col-xs-offset-3'></div>");
+
+            $('#message').html("<h4>Thank you for joining our newsletter list!</h4>")
+                .append("<h5>Check your email, confirm it, and you will start seeing exclusive deals and news.</h5>")
+                .hide()
+                .fadeIn(1500, function() {
+                    $('#message').append("<div></div>");
+                });
+
+        }
+    });
+});
+
+
 /*
 
 // Events (jQuery).
